844e93f6e62db22d7f0f6257ddc495bcc45cb3cc
phoenixlord <yacham09@yahoo.com>
2018-10-23 19:38:59 +0100

    socket io coomit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path=require('path');
+const http=require('http')
 const express=require('express')
+const socketIO=require('socket.io')
 
 
 var app=express()
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('New user connected')
+    
+     socket.on('disconnect',()=>{
+            console.log('disconnected from client')
+        })
+})
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`)
 })
 
