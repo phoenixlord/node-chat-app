4f658e53e2a6127786d34b657e1f4f0c62c15ba0
phoenixlord <yacham09@yahoo.com>
2018-10-24 12:07:19 +0100

    js file commited

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+  var socket = io();
+
+  socket.on('connect', function () {
+      console.log('connected to server')
+      
+      
+      
+  })
+
+  socket.on('disconnect', function () {
+      console.log('Disconnected from server')
+  })
+
+  socket.on('newMessage',function(message){
+      console.log('New message',message)
+  })
